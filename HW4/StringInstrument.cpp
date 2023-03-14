9b9956eed501de36bac6e99baf511fb3c580e099
Anderson Levasseur <anderson.levasseur@gmail.com>
2023-03-14 11:37:07 -0400

    Initial Commit

---
+++ HW4/StringInstrument.cpp
@@ -0,0 +1,9 @@
+#include "StringInstrument.h"
+
+// TODO: Define mutator functions - 
+//      SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
+
+
+// TODO: Define accessor functions -
+//      GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+
